44869323e0b53c545b21a53dc0e7ee676f0aae22
Riteshbhalerao11 <ritesh.bhalerao.11603@gmail.com>
2025-10-29 01:47:38 +0530

    added token length cap

--- steering/steer_model.py
+++ steering/steer_model.py
@@ -220,7 +220,9 @@ def steer_and_generate(
             add_special_tokens=False, 
             return_tensors="pt", 
             padding=True,
-            padding_side="left"
+            padding_side="left",
+            truncation=True,
+            max_length=1024,
         ).to("cuda")
         
         tok_batches.append(tok_batch)
